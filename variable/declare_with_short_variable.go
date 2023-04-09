cfc8f04572a53867feeb489f641082c1764f3b3d
tuannt20 <tuango.472000@gmail.com>
2023-04-09 21:12:29 +0700

    :sparkles: feat: Learn declare short variable in Go
    feat: Learn declare short variable in Go

---
+++ variable/declare_with_short_variable.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Khi khai báo biến bằng cách := thì Golang sẽ tự động suy ra kiểu dữ liệu dựa trên giá trị được gán
+	// Không thể khai báo thêm kiểu dữ liệu cho cách khai báo :=
+	// Khai báo bằng := thì biến chỉ có giá trị bên trong hàm (giống local scope bên JS)
+	// Khai báo bằng := thì không thể thay đổi giá trị của biến
+	a := 5
+
+	fmt.Println(a)
+}
